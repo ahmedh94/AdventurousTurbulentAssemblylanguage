893ddf8a64811ffb08544835bdf3845cf4fa35d0
Ahmed Hasan <134266773+ahmedh94@users.noreply.github.com>
2025-06-03 13:43:33 +0000

    comm-2

--- main.c
+++ main.c
@@ -1,5 +1,15 @@
+/*
 #include <stdio.h>
 
-int main(void) {
+void main(void) {
   printf("Hello ahmed");
+}
+*/
+
+#include <stdio.h>
+
+void main(void) {
+  printf("i'm ahmed \n");
+  printf("25-06-1994 \n");
+  printf("2025 \n");
 }
